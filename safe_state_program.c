800d31034627a8ccd0bc6631cbc6add26a0a6e6e
PratyakshRana <63449908+PratyakshRana@users.noreply.github.com>
2020-04-10 20:57:34 +0530

    Update safe_state_program.c

--- safe_state_program.c
+++ safe_state_program.c
@@ -73,17 +73,3 @@ int main()
 		printf("The System is not in Safe State");
 	}
 }
-
-
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
